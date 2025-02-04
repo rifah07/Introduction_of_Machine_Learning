68faed93ae5dedf83c26e46cf1082d21188c2974
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-02-04 08:12:00 +0600

    code updated

--- imageToCSV/image_to_csv.py
+++ imageToCSV/image_to_csv.py
@@ -8,25 +8,24 @@ import pytesseract
 from PIL import Image
 import csv
 
-# Path to your image file
 image_path = "image.jpeg"
 output_csv = "generated_csv.csv"
 
-# Convert image to text using Tesseract
+#convert image to text using Tesseract
 def extract_text_from_image(image_path):
     img = Image.open(image_path)
     text = pytesseract.image_to_string(img, lang="ben+eng")  # Specify both Bengali (ben) and English (eng) for better OCR
     return text
 
-# Save extracted text to CSV
+#save extracted text to CSV
 def save_text_to_csv(text, output_csv):
     with open(output_csv, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        # Split text by line and write each line to CSV
+        #split text by line and write each line to CSV
         for line in text.splitlines():
             writer.writerow([line])
 
-# Main processing
+#main processing
 text = extract_text_from_image(image_path)
 save_text_to_csv(text, output_csv)
 
